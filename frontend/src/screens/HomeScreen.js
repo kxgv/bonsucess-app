c2bf5595f561acb44ef91ffab28521aa60d0524f
kxgv <guinakevin@gmail.com>
2020-12-19 19:16:23 +0100

    data uploaded from backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Product } from '../components/Product';
-import data from '../data';
+import axios from 'axios'; 
 
 /** 
  * Returns products inside row in Home Screen
@@ -21,7 +21,7 @@ export default function HomeScreen() {
   return (
     <div>
       <div className="row center">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <Product key={product._id} product={product}></Product>
         ))}
       </div>
