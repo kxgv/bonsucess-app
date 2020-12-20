271164241bc3c9abcabfa942bc11a3d775e86ebc
kxgv <guinakevin@gmail.com>
2020-12-20 19:15:58 +0100

    new api to get products, new product screen redux added and new constants and actions for redux react view

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,18 @@ import data from './data.js';
 
 const app = express();
 
+
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find(x => x._id === req.params.id); 
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product not Found'}); 
+  }
+  res.send(data.products);
+});
+
+
 /**
  * mongoose.connect('mongodb://localhost/bonsucess', {
   useNewUrlParser: true,
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,65 +1,79 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import data from '..//data';
-
-/**Returns a Screen for a Product
- * @function
- * @name ProductScreen
- * @param {*} props - any property from Product Component
- */
+import { detailsProduct } from '../actions/productActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function ProductScreen(props) {
-  const product = data.products.find((x) => x._id === props.match.params.id);
-  if (!product) {
-    return <div> Producto no encontrado</div>;
-  }
+  const dispatch = useDispatch();
+  const productId = props.match.params.id;
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+
+  useEffect(() => {
+    dispatch(detailsProduct(productId));
+  }, [dispatch, productId]);
+
   return (
     <div>
-      <Link to="/">Back to result</Link>
-      <div className="row top">
-        <div className="col-2">
-          <img className="large" src={product.image} alt={product.name}></img>
-        </div>
-        <div className="col-1">
-          <ul>
-            <li>
-              <h1>{product.name}</h1>
-            </li>
-            <li>Precio: {product.price}</li>
-            <li>
-              Descripción:
-              <p>{product.description}</p>
-            </li>
-          </ul>
-        </div>
-        <div className="col-1">
-          <div className="card card-body">
-            <ul>
-              <li>
-                <div className="row">
-                  <div>Price</div>
-                  <div className="price">${product.price}</div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+            <div>
+              <Link to="/">Volver</Link>
+              <div className="row top">
+                <div className="col-2">
+                  <img
+                    className="large"
+                    src={product.image}
+                    alt={product.name}
+                  ></img>
                 </div>
-              </li>
-              <li>
-                <div className="row">
-                  <div>Estado</div>
-                  <div>
-                    {product.countInStock > 0 ? (
-                      <span className="success">En Stock</span>
-                    ) : (
-                      <span className="danger">No disponible</span>
-                    )}
+                <div className="col-1">
+                  <ul>
+                    <li>
+                      <h1>{product.name}</h1>
+                    </li>
+                    <li>Precio: ${product.price}</li>
+                    <li>
+                      Descripción:
+                  <p>{product.description}</p>
+                    </li>
+                  </ul>
+                </div>
+                <div className="col-1">
+                  <div className="card card-body">
+                    <ul>
+                      <li>
+                        <div className="row">
+                          <div>Price</div>
+                          <div className="price">${product.price}</div>
+                        </div>
+                      </li>
+                      <li>
+                        <div className="row">
+                          <div>Estado</div>
+                          <div>
+                            {product.countInStock > 0 ? (
+                              <span className="success">En Stock</span>
+                            ) : (
+                                <span className="danger">No disponible</span>
+                              )}
+                          </div>
+                        </div>
+                      </li>
+                      <li>
+                        <button className="primary block">Añadir a la cesta</button>
+                      </li>
+                    </ul>
                   </div>
                 </div>
-              </li>
-              <li>
-                <button className="primary block">Añadir a la cesta</button>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+              </div>
+            </div>
+          )}
     </div>
   );
 }
